163822f0c4417a145dcf5594de770da672cb9026
+ <familymanjosh@gmail.com>
2023-02-26 13:30:27 -0800

    Hi there

--- index.js
+++ index.js
@@ -1,1 +1,37 @@
-// Write your solution here!
+// beforeReach(function() {
+//     cats.length = 0;
+//     cats.push("Milo","Otis", "Garfield")
+// })
+const cats = ["Milo","Otis", "Garfield"]
+
+function destructivelyAppendCat(name){
+    cats.push(name)
+    return cats
+}
+function  destructivelyPrependCat(name){
+    cats.unshift(name)
+    return cats
+}
+function destructivelyRemoveLastCat(){
+    cats.pop()
+    return cats
+}
+function destructivelyRemoveFirstCat(){
+    cats.shift()
+    return cats
+}
+
+function  appendCat(name){
+    const allCats= [...cats, name]
+    return allCats
+}
+function prependCat(name){
+   const buddyCats = [name, ...cats]
+    return buddyCats
+}
+function removeLastCat(){
+    return cats.slice(0,-1)
+}
+function removeFirstCat(){
+    return cats.slice(1)
+}
